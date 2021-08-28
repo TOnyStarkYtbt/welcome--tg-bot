ed28ddb955e44abbf00aa4ab7026ec1a85804dd2
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-28 17:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from telegram import Update
 from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
 import os
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-Token =os.environ.get("MT_BOT_TOKEN",None)
+Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
-def start(updater,context):
- updater.message.reply_text('''👋Hi Bro or Sis Iam Welcome Messanger bot\n\n👤Any Doubt - @Mo_Tech_YT\n🔊Bot Updates - @Mo_Tech_YT\n\n😮More Details Clcik /help Button''')
 
-def help(updater,context):
- updater.message.reply_text("👇English👇\n\n⚕️Add ME TO YOUR GROUP\n⚕️MAKE ME AS ADMIN ON GROUP\n\n👇Malayalam👇\n\n⚕️ആദ്യം എന്നെ നിങ്ങളുടെ ഗ്രൂപ്പിൽ ആഡ് ആകൂ\n⚕️എന്നെ നിങളുടെ ഗ്രൂപ്പിൽ അഡ്മിൻ ആകൂ\n\n🖥️HOW TO OWN🖥️\nhttps://youtu.be/0a5nnEj5BjY")
+def start(updater,context):
+    keyboard = 
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/HTechMedia'),
+        InlineKeyboardButton('Support😎', url='https://telegram.me/HTechMediaSupport')
+        ],[
+        InlineKeyboardButton('Devoloper👀', url='https://telegram.me/NxtStark'),
+        InlineKeyboardButton('Website🪐', url='https://htechmediayt.wixsite.com/htechmedia'),
+        InlineKeyboardButton('Youtube🚀', url='https:/youtube.com/c/HTechMedia')
+        ]]
+   
+    reply_markup = InlineKeyboardMarkup(keyboard)
+   
+    updater.message.reply_text('''👋Hi Bro Iam Welcome Messanger bot\nAdd Me In Your Group And See My Pever🔥\n😮More Details Clcik /help Button\n\nMade By @HTechMedia''')
+
+ def help(updater,context):
+ updater.message.reply_text("⚕️Add ME TO YOUR GROUP\n⚕️MAKE ME AS ADMIN ON GROUP")
  
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'👋Hello {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖')
+        update.message.reply_text(f'👋Hello {member.full_name} , Welcome to {group.full_name}\n\nThank You For Joining💖')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
